3c41ca308274460c1129ef4dd0b5a99bd479b48b
Reynard <reynardets@gmail.com>
2021-05-10 15:20:21 +0700

    #LogoAndSplash

---
+++ app/src/main/java/com/example/beesafe/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.beesafe
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.example.beesafe.databinding.ActivitySplashBinding
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fadein = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        binding.splashText.startAnimation(fadein)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
