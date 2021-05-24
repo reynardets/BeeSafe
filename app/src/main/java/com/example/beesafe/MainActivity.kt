0fc4898db979dc6e48d9229ef1e3c9ccd7f7a4e7
Favian Pratista <farelony@gmail.com>
2021-05-24 18:25:20 +0700

    Fixing Main Activity
    Erasing appbarConfiguration which give null pointer exception on the ActionBar

--- app/src/main/java/com/example/beesafe/MainActivity.kt
+++ app/src/main/java/com/example/beesafe/MainActivity.kt
@@ -18,9 +18,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_lapor, R.id.navigation_akun))
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
