97188a9928efddb62b6ff45919205669e91fb916
raihan haikal <rhraihanhaikal@gmail.com>
2021-05-27 13:18:25 +0700

    Preprocess Library

---
+++ Sastrawi/Dictionary/DictionaryInterface.py
@@ -0,0 +1,5 @@
+class DictionaryInterface(object):
+    """Interface definition of dictionary"""
+
+    def contains(self, word):
+        raise NotImplementedError('you must implement this method manually')
